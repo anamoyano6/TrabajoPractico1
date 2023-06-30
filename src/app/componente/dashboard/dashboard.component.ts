8a4085ec4c6b8f12e41284c978ef15b7cfb3a862
anamoyano6 <104173512+anamoyano6@users.noreply.github.com>
2023-06-30 12:00:37 -0300

    event binding

--- src/app/componente/dashboard/dashboard.component.ts
+++ src/app/componente/dashboard/dashboard.component.ts
@@ -9,17 +9,28 @@ export class DashboardComponent {
 
   titulo: string;
   tituloPB: string;
+  tituloEB: string;
   contenido: string;
   alumnos: string;
+  profesor: string;
   imagen: string;
+  textoEB: string;
+
 
   constructor() {
     this.titulo = "ANGULAR";
     this.contenido = "Es un framework de codigo abierto y permite desarrollar paginas web de una sola pagina.";
     this.alumnos = "Ana Paula Moyano";
+    this.profesor = "Nicolas Vassallo"
     this.tituloPB = "PROPERTY BINDING";
     this.imagen = "https://www.mediasource.mx/hubfs/blog-files/fondos-web.jpg#keepProtocol";
-    
-   
+    this.tituloEB = "EVENT BINDING";
+    this.textoEB = "Integrantes";
+  
   }
+  cambiartexto() {
+    this.textoEB = "Ana Moyano";
+  }
+
+
   }
